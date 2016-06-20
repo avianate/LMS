9c8dab0ceca6765a7ca6aa230cbb5932642f9252
Graham <grahamn@corp.nt.navitaire.com>
2016-06-20 13:30:47 -0600

    Update seeder

--- Migrations/Seeder.cs
+++ Migrations/Seeder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using LMS.Data;
 using LMS.Entities;
@@ -18,6 +19,21 @@ namespace LMS.Migrations
         {
             if (!_context.Posts.Any())
             {
+                var postTags = new List<PostTag>
+                {
+                    new PostTag { PostId = 1, TagId = 1 }
+                };
+
+                var postCategories = new List<PostCategory>
+                {
+                    new PostCategory { PostId = 1, CategoryId = 1 }
+                };
+
+                var postComments = new List<PostComment> 
+                {
+                    new PostComment { PostId = 1, CommentId = 1 }
+                };
+
                 var post = new Post()
                 {
                       Title = "First Post",
@@ -26,21 +42,26 @@ namespace LMS.Migrations
                       Slug = "first-post",
                       PublishDate = DateTime.UtcNow,
                       IsPublished = true,
-                      ModifiedDate = DateTime.UtcNow
+                      ModifiedDate = DateTime.UtcNow,
+                      PostTags = postTags,
+                      PostCategories = postCategories,
+                      PostComments = postComments
                 };
 
                 _context.Posts.Add(post);
                 
                 var tag = new Tag()
                 {
-                    TagName = "Tag"
+                    TagName = "Tag",
+                    PostTags = postTags
                 };
 
                 _context.Tags.Add(tag);
 
                 var category = new Category()
                 {
-                    CategoryName = "Category"
+                    CategoryName = "Category",
+                    PostCategories = postCategories
                 };
 
                 _context.Categories.Add(category);
@@ -51,7 +72,8 @@ namespace LMS.Migrations
                     PublishDate = DateTime.UtcNow,
                     IsApproved = true,
                     UserEmail = "a@b.com",
-                    UserName = "username"
+                    UserName = "username",
+                    PostComments = postComments
                 };
 
                 _context.Comments.Add(comment);
